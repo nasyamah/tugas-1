2e2cc338aa1d33fdfee4b378b51cc27b89c6dad8
nasyamah <48594340+nasyamah@users.noreply.github.com>
2019-03-23 19:21:29 +0700

    Create helo.php

---
+++ helo.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>information retrieval unisbank</title>
+		<style>
+			h1 {
+			color: black;
+			text-align: center;
+			margin-top: 10%;
+			   }
+		</style>
+	</head>
+	<body>
+		<h1> <?php echo 'Hello World <br>'; 
+		echo 'Fitri Arisanti Dewi 16.01.55.0083'?> ;
+		</h1>
+	</body>
+</html>
